03aeef637b84a99a5e5d6f96672e9ffc5e46d06d
Ryan Francesconi <thiscomputermagic@gmail.com>
2026-07-19 17:11:35 -0700

    Fix source file copyright headers to reference this package
    Headers were pointing at the wrong repo (often the package this one
    was split out of), missing the /ryanfrancesconi/<repo> revision-history
    URL entirely, or missing the header line altogether.

--- Sources/SPFKMetadataBase/TagProperties/TagKey+ID3.swift
+++ Sources/SPFKMetadataBase/TagProperties/TagKey+ID3.swift
@@ -1,4 +1,4 @@
-// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-metadata
+// Copyright Ryan Francesconi. All Rights Reserved. Revision History at https://github.com/ryanfrancesconi/spfk-metadata-base
 
 import Foundation
 
